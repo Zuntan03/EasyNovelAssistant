f69791edf93ac3b32c5ef64375f9a24b4b8d58a2
Zuntan <121656336+Zuntan03@users.noreply.github.com>
2024-04-24 21:36:26 +0900

    support for linux #1 https://github.com/Zuntan03/EasyNovelAssistant/issues/1#issuecomment-2074649135

---
+++ Run-EasyNovelAssistant.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+mkdir -p sample
+cd sample
+curl  -LO https://yyy.wpx.jp/EasyNovelAssistant/sample/tutorial.json
+
+if [ $? -ne 0 ]; then
+    exit 1
+fi
+
+curl  -LO https://yyy.wpx.jp/EasyNovelAssistant/sample/sample.json
+if [ $? -ne 0 ]; then
+    exit 1
+fi
+
+if [ -e "nsfw.json" ]; then
+    curl  -LO https://yyy.wpx.jp/EasyNovelAssistant/sample/sample.json
+fi
+
+cd -
+
+source ./venv/bin/activate
+python ./EasyNovelAssistant/src/easy_novel_assistant.py
